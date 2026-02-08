af2dd92686029b2749060b43fa136fa7edd8c70f
Vishant Bhardwaj <155170784+vishu2724@users.noreply.github.com>
2026-02-08 20:57:57 +0530

    Change API base URL to production endpoint

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://socialx-jaxy.onrender.com/api",
 });
 
 // 🔐 attach token automatically
