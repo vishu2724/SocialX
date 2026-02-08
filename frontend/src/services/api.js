36b0e68cae53c7f824ca0b3b8d93020cb7141357
Vishant Bhardwaj <bhardwajvishant52@gmail.com>
2026-02-08 21:10:43 +0530

    frontend: vercel ready 2

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://socialx-jaxy.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
-// 🔐 attach token automatically
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
   if (token) {
